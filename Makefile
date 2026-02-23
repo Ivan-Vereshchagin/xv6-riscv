f3f8e52ca38b057d6140b72059b759c86bc381dd
Ivan Vereshchagin <iveresagin932@gmail.com>
2026-02-23 19:14:41 +0300

    Задание 1

--- Makefile
+++ Makefile
@@ -145,6 +145,7 @@ UPROGS=\
 	$U/_logstress\
 	$U/_forphan\
 	$U/_dorphan\
+	$U/_proc_task\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
---
+++ user/proc_task.c
@@ -0,0 +1,61 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  int pid;
+  int wpid;
+  int status = 0;
+  int variant = 0;
+  
+  if (argc > 1) {
+    if (argv[1][0] == 'b' || argv[1][0] == 'B' || argv[1][0] == '1') variant = 1;
+  }
+  
+  pid = fork();
+  
+  if (pid < 0) {
+    write(2, "Fork error: fail\n", 18);
+    exit(1);
+  }
+  
+  if (pid == 0) {
+    pause(500);
+    exit(1);
+  } else {
+    printf("Parent ID: %d, child ID: %d\n", getpid(), pid);
+    
+    if (variant == 0) {
+      wpid = wait(&status);
+      
+      if (wpid < 0) {
+        write(2, "Wait error: fail\n", 18);
+        exit(1);
+      }
+      
+      printf("Child %d terminated, exit code: %d\n", wpid, status);
+      
+    } else {
+      
+      pause(20);
+
+      if (kill(pid) < 0) {
+        write(2, "Kill error: fail\n", 18);
+        exit(1);
+      }
+      
+      wpid = wait(&status);
+      
+      if (wpid < 0) {
+        write(2, "Wait error: fail\n", 18);
+        exit(1);
+      }
+      
+      printf("Child %d terminated, exit code: %d\n", wpid, status);
+    }
+    exit(0);
+  } 
+  exit(0);
+}
